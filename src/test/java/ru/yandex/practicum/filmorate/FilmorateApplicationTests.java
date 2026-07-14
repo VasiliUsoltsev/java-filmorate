c2af18c63b5b30beeca1d2018f6f28ececa7d835
Vasili Usoltsev <vasili.n.usoltsev@yandex.com>
2026-07-15 03:03:45 +0700

    3-я итерация финального задания по 10 спринту

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FilmorateApplicationTests {
